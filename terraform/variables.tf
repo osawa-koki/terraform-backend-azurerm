daca9553b5dfcc27676bc182e1393c992135145e
osawa-koki <github@osawa.dev>
2023-03-19 00:39:12 +0900

    tf変数。

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+
+variable "project_name" {
+  type        = string
+  description = "The name of the project"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "The subscription ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "The client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "The client secret"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "The tenant ID"
+}
